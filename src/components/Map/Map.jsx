7d13a1d881fbc1f280c1bed60b7d8a004a785255
Lonly <alexleonov011@gmail.com>
2024-08-26 15:34:13 +0300

    map and team comp

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,3 +1,16 @@
+import styles from './Map.module.scss';
+import Image from 'next/image';
+
+
 export default function Map() {
-	return <div>Map</div>;
+	return <div className={styles.wrap}>
+		<Image
+			src="/map.svg"
+			alt="map"
+			className={styles.mapImg}
+			width={1920}
+			height={1102}
+		/>
+
+	</div>;
 }
