0109e758fd7c662d439f14c1025da5c0a16f1070
Nikita Averyanov <rac1onal@yandex.ru>
2024-08-27 12:06:07 +0300

    feat: add the necessary dropdown menu functionality

---
+++ src/data/menuItems.js
@@ -0,0 +1,66 @@
+export const menuItems = {
+	about: {
+		title: 'О нас',
+		items: [
+			{ href: '/about/quality-policy', text: 'Политика в области качества' },
+			{ href: '/about/corporate-ethics', text: 'Кодекс корпоративной этики' },
+			{
+				href: '/about/safety-and-health-policy',
+				text: 'Политика в области охраны труда',
+			},
+			{ href: '/about/sout', text: 'Ведомости СОУТ' },
+			{
+				href: '/about/personal-data-processing-policy',
+				text: 'Политика обработки персональных данных',
+			},
+		],
+	},
+	applications: {
+		title: 'Применения',
+		items: [
+			{
+				href: '/applications/automobile-industry',
+				text: 'Автомобильная промышленность',
+			},
+			{
+				href: '/applications/manufacture-of-household-appliances',
+				text: 'Производство бытовой техники',
+			},
+			{
+				href: '/applications/furniture-industry',
+				text: 'Мебельная промышленность',
+			},
+			{
+				href: '/applications/electronics-manufacturing',
+				text: 'Производство электроники',
+			},
+			{
+				href: '/applications/construction-and-renovation',
+				text: 'Строительство и ремонт',
+			},
+			{ href: '/applications/transport', text: 'Транспорт' },
+			{ href: '/applications/ros', text: 'РОС' },
+		],
+	},
+	goods: {
+		title: 'Товары',
+		items: [
+			{
+				href: '/goods/foamed-double-sided-adhesive-tapes',
+				text: 'Вспененные двусторонние клейкие ленты',
+			},
+			{
+				href: '/goods/thin-double-sided-adhesive-tapes',
+				text: 'Тонкие двусторонние клейкие ленты',
+			},
+			{
+				href: '/goods/single-sided-adhesive-tapes',
+				text: 'Односторонние клейкие ленты',
+			},
+			{ href: '/goods/primers-and-cleaners', text: 'Праймеры и очистители' },
+			{ href: '/goods/tapes', text: 'Пленки' },
+			{ href: '/goods/tools', text: 'Инструменты' },
+			{ href: '/goods/rubbers', text: 'Резины' },
+		],
+	},
+};
