6be5d1370745990642a6f739cbd5f97a9484fb2a
Lonly-Lokly <102942172+Lonly-Lokly@users.noreply.github.com>
2024-08-27 18:16:10 +0300

    some requisites comp

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,3 +1,9 @@
+import Requisites from './../../components/about/Requisites/Requisites.jsx';
+
 export default function About() {
-	return <h1>About page</h1>;
+	return <div>
+		<h1>About page</h1>
+		<Requisites></Requisites>
+	</div>;
+
 }
