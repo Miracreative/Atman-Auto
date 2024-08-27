bcfde304ddf9772e9cf52764f8bb1a62474118c4
Nikita Averyanov <rac1onal@yandex.ru>
2024-08-27 20:15:41 +0300

    feat: add some settings with next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		formats: ['image/avif', 'image/webp'],
+	},
+	swcMinify: true,
+};
 
 export default nextConfig;
