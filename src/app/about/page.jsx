b6359bb71490a3511868490bf101f3fbbb7745e8
Lonly <alexleonov011@gmail.com>
2024-08-28 10:22:21 +0300

    requisites copm done

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,21 @@
+import Header from './../../components/Header/Header.jsx';
+import Team from './../../components/Team/Team.jsx';
+import Map from './../../components/Map/Map.jsx';
 import Requisites from './../../components/about/Requisites/Requisites.jsx';
+import Footer from './../../components/Footer/Footer.jsx';
 
 export default function About() {
-	return <div>
-		<h1>About page</h1>
-		<Requisites></Requisites>
-	</div>;
+	return (
+		<>
+			<Header></Header>
+			<main>
+				<h1>About page</h1>
 
+				<Team></Team>
+				<Map></Map>
+				<Requisites></Requisites>
+			</main>
+			<Footer></Footer>
+		</>
+	);
 }
