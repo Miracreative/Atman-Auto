5af781290cace8af8a5f2a0f274439e6ea694bc5
Nikita Averyanov <110771527+Racio-begin@users.noreply.github.com>
2024-08-30 13:13:36 +0300

    feat: add smart scroll to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,58 @@
+// 'use client';
+
+// import { useState } from 'react';
+// import Link from 'next/link';
+
+// import HeaderLogo from '../HeaderLogo/HeaderLogo';
+// import HeaderNav from '../HeaderNav/HeaderNav';
+// import BurgerButton from './../BurgerButton/BurgerButton';
+// import BurgerMenu from './../BurgerMenu/BurgerMenu';
+
+// import { contacts } from '../../data/contacts';
+
+// import styles from './Header.module.scss';
+
+// export default function Header() {
+// 	const [activeDropdown, setActiveDropdown] = useState(null);
+// 	const [menuActive, setMenuActive] = useState(false);
+
+// 	const toggleDropdown = (name) => {
+// 		setActiveDropdown((prevActiveDropdown) =>
+// 			prevActiveDropdown === name ? null : name,
+// 		);
+// 	};
+
+// 	const toggleMenu = () => {
+// 		setMenuActive((prevMenuActive) => !prevMenuActive);
+// 	};
+
+// 	return (
+// 		<header className={styles.header}>
+// 			<HeaderLogo />
+// 			<HeaderNav
+// 				toggleDropdown={toggleDropdown}
+// 				activeDropdown={activeDropdown}
+// 			/>
+// 			<Link className={styles.number} href={`tel:${contacts.phone}`}>
+// 				{contacts.phone}
+// 			</Link>
+// 			<BurgerButton
+// 				isActive={menuActive}
+// 				toggleMenu={toggleMenu}
+// 			></BurgerButton>
+// 			<BurgerMenu
+// 				isActive={menuActive}
+// 				setActive={setMenuActive}
+// 				toggleDropdown={toggleDropdown}
+// 				activeDropdown={activeDropdown}
+// 			></BurgerMenu>
+// 		</header>
+// 	);
+// }
+
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 
 import HeaderLogo from '../HeaderLogo/HeaderLogo';
@@ -15,6 +67,8 @@ import styles from './Header.module.scss';
 export default function Header() {
 	const [activeDropdown, setActiveDropdown] = useState(null);
 	const [menuActive, setMenuActive] = useState(false);
+	const [isVisible, setIsVisible] = useState(true);
+	const [lastScrollY, setLastScrollY] = useState(0);
 
 	const toggleDropdown = (name) => {
 		setActiveDropdown((prevActiveDropdown) =>
@@ -26,8 +80,32 @@ export default function Header() {
 		setMenuActive((prevMenuActive) => !prevMenuActive);
 	};
 
+	const handleScroll = () => {
+		const currentScrollY = window.scrollY;
+
+		if (currentScrollY > 200) {
+			if (currentScrollY > lastScrollY) {
+				setIsVisible(false);
+			} else {
+				setIsVisible(true);
+			}
+		} else {
+			setIsVisible(true);
+		}
+
+		setLastScrollY(currentScrollY);
+	};
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, [lastScrollY]);
+
 	return (
-		<header className={styles.header}>
+		<header className={`${styles.header} ${!isVisible ? styles.hidden : ''}`}>
 			<HeaderLogo />
 			<HeaderNav
 				toggleDropdown={toggleDropdown}
