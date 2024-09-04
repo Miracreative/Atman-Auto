b1181c9261aed7d40de9c5478fccee3932f353cb
Lonly <alexleonov011@gmail.com>
2024-09-04 18:07:12 +0300

    the application page is done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ import socialLinks from '@/data/socialLinks';
 
 export default function Footer() {
 	return <footer className={styles.footer}>
-		<div className="container-delete">
+		<div className="container">
 			<div className={styles.content}>
 				<div className={styles.textWrap}>
 					<div className={styles.title}>
