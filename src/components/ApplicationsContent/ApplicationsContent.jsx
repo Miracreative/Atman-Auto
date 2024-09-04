66de1dc7684bbc2fdfd82da07214d898a639b839
Lonly <alexleonov011@gmail.com>
2024-09-04 09:31:29 +0300

    small fix

---
+++ src/components/ApplicationsContent/ApplicationsContent.jsx
@@ -0,0 +1,20 @@
+import styles from './ApplicationsContent.module.scss';
+import Image from 'next/image';
+
+
+export default function ApplicationsContent() {
+	return (
+		<>
+			<section className={styles.section}>
+				<div className="container">
+
+					<div className={styles.wrap}>
+						hi bob
+					</div>
+
+
+				</div>
+			</section>
+		</>
+	)
+}
