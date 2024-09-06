321c2734e9a2e9a82dc4236910885fc318fe33a5
Nikita Averyanov <110771527+Racio-begin@users.noreply.github.com>
2024-09-06 15:06:26 +0300

    feat: to make up the first screen of the Decisions section

--- src/components/Decisions/Decisions.jsx
+++ src/components/Decisions/Decisions.jsx
@@ -1,3 +1,149 @@
+'use client';
+
+import Link from 'next/link';
+import { useState } from 'react';
+
+import ArrowButton from '../ArrowButton/ArrowButton';
+
+import styles from './Decisions.module.scss';
+
 export default function Decisions() {
-	return <div>Decisions</div>;
+	const [checked, setChecked] = useState('task-1');
+
+	const handleChecked = (e) => {
+		setChecked(e.target.value);
+	};
+
+	return (
+		<section className={styles.section}>
+			<div className="container">
+				<div className={styles.content}>
+					<div className={styles.titleContainer}>
+						<h2 className={styles.title}>
+							Выберите продукт, который соответствует вашим потребностям
+						</h2>
+						<p className={styles.description}>
+							Узнайте, какой тип ленты лучше всего подходит для вашего случая
+							использования, ответив на несколько вопросов.
+						</p>
+						<div className={styles.counter}>
+							01<span>/05</span>
+						</div>
+					</div>
+					<div className={styles.survey}>
+						<div className={styles.surveyForm}>
+							<div className={styles.surveyTitle}>
+								Какая задача стоит перед Вами?
+							</div>
+							<ul className={styles.options}>
+								<li className={styles.option}>
+									<input
+										type="radio"
+										name="tasks"
+										id="task-1"
+										value="task-1"
+										checked={checked === 'task-1'}
+										onChange={handleChecked}
+									/>
+									<label htmlFor="task-1">Соединение деталей/материалов</label>
+								</li>
+								<li className={styles.option}>
+									<input
+										type="radio"
+										name="tasks"
+										id="task-2"
+										value="task-2"
+										checked={checked === 'task-2'}
+										onChange={handleChecked}
+									/>
+									<label htmlFor="task-2">Ламинация материалов</label>
+								</li>
+								<li className={styles.option}>
+									<input
+										type="radio"
+										name="tasks"
+										id="task-3"
+										value="task-3"
+										checked={checked === 'task-3'}
+										onChange={handleChecked}
+									/>
+									<label htmlFor="task-3">
+										Крепление прокладок и уплотнителей
+									</label>
+								</li>
+								<li className={styles.option}>
+									<input
+										type="radio"
+										name="tasks"
+										id="task-4"
+										value="task-4"
+										checked={checked === 'task-4'}
+										onChange={handleChecked}
+									/>
+									<label htmlFor="task-4">Создание разъемного соединения</label>
+								</li>
+								<li className={styles.option}>
+									<input
+										type="radio"
+										name="tasks"
+										id="task-5"
+										value="task-5"
+										checked={checked === 'task-5'}
+										onChange={handleChecked}
+									/>
+									<label htmlFor="task-5">Герметизация соединения</label>
+								</li>
+								<li className={styles.option}>
+									<input
+										type="radio"
+										name="tasks"
+										id="task-6"
+										value="task-6"
+										checked={checked === 'task-6'}
+										onChange={handleChecked}
+									/>
+									<label htmlFor="task-6">Защита поверхности</label>
+								</li>
+								<li className={styles.option}>
+									<input
+										type="radio"
+										name="tasks"
+										id="task-7"
+										value="task-7"
+										checked={checked === 'task-7'}
+										onChange={handleChecked}
+									/>
+									<label htmlFor="task-7">
+										Подготовка поверхности для склеивания
+									</label>
+								</li>
+								<li className={styles.option}>
+									<input
+										type="radio"
+										name="tasks"
+										id="task-8"
+										value="task-8"
+										checked={checked === 'task-8'}
+										onChange={handleChecked}
+									/>
+									<label htmlFor="task-8">Прочее</label>
+								</li>
+							</ul>
+						</div>
+						<div className={styles.controls}>
+							<button className={styles.backButton}>
+								<ArrowButton backgroundColor="transparent" opacity={0.2} />
+							</button>
+
+							{/* <ArrowButton /> */}
+							<button className={`${styles.forwardButton} button`}>
+								Продолжить
+							</button>
+							<Link href="/"></Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
