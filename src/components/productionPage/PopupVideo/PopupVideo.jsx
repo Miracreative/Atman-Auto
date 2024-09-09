648d7b22eed937a7ea0aa68293afbc7af9e5f88f
Lonly <alexleonov011@gmail.com>
2024-09-09 12:44:33 +0300

    small fix production content

---
+++ src/components/productionPage/PopupVideo/PopupVideo.jsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import styles from './PopupVideo.module.scss';
+
+export default function PopupVideo() {
+	return (
+		<section className={styles.section}>
+			hi Bob
+		</section>
+	);
+}
