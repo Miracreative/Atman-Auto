8d4ed79062d7df9f2d710c4a9382795f27348a2a
Lonly <alexleonov011@gmail.com>
2024-09-16 19:08:48 +0300

    added news content

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,15 +1,15 @@
 import PageTitle from './../../components/PageTitle/PageTitle.jsx';
+import NewsContent from '@/components/newsPage/NewsContent/NewsContent.jsx';
 
 export default function News() {
 	return (
 		<>
 
-			<h1>News page</h1>
 			<PageTitle
 				title='Новости'
 
 				text={<div>События в отрасли <br /> и достижения компании </div>}></PageTitle>
-
+			<NewsContent></NewsContent>
 
 		</>
 	);
