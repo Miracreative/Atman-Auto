c0589b7cfa3852b68d48356ab7882fb7019a5a2f
Nikita Averyanov <110771527+Racio-begin@users.noreply.github.com>
2024-09-18 16:54:53 +0300

    feat: turn over page 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,6 +2,12 @@
 
 import { useRouter } from 'next/navigation';
 import styles from '@/scss/not-found.module.scss';
+import Image from 'next/image';
+
+import background from '/public/not-found/404.png';
+import image from '/public/not-found/404-image.png';
+import logoImage from '/public/white-logo.svg';
+import HeaderLogo from '@/components/headerSection/HeaderLogo/HeaderLogo';
 
 export default function NotFound() {
 	const router = useRouter();
@@ -12,12 +18,31 @@ export default function NotFound() {
 
 	return (
 		<div className={styles.container}>
-			<p className={styles.code}>404</p>
-			<h2 className={styles.title}>Страница не найдена</h2>
-			<p className={styles.description}>Не удалось найти запрошенный ресурс</p>
-			<button className={`${styles.button} button`} onClick={handleButtonClick}>
-				На главную
-			</button>
+			<div className={styles.background}>
+				<div className={styles.content}>
+					{/* <Image
+				className={styles.background}
+				src={background}
+				alt="Background image - 404"
+				/> */}
+					<Image
+						src={logoImage}
+						alt="Atman Auto logo"
+						className={styles.logo}
+						// priority
+					/>
+					<Image className={styles.image} src={image} alt="Image - 404" />
+					<h1 className={styles.title}>
+						Эта страница удалена или находится в работе
+					</h1>
+					<button
+						className={`${styles.button} button`}
+						onClick={handleButtonClick}
+					>
+						На главную
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 }
