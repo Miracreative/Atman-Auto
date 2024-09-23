a768bbf9f7dbd79cbed89bc644a4717f2f295418
Nikita Averyanov <110771527+Racio-begin@users.noreply.github.com>
2024-09-23 17:46:54 +0300

    feat: make up an adaptive Product page

--- src/components/goodsPage/Showcase/Showcase.jsx
+++ src/components/goodsPage/Showcase/Showcase.jsx
@@ -1,39 +1,35 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
-
-import axios from 'axios';
-
 import GoodsSlider from '../GoodsSlider/GoodsSlider.jsx';
-
-import URL from '@/constants/url.js';
 
 import styles from './Showcase.module.scss';
 
 const Showcase = ({ products }) => {
+	const product = products[2];
+
 	return (
 		<section className={styles.section}>
 			<div className={styles.titleContainer}>
 				<div className={styles.breadCrumbs}>
 					Главная = Товары =
 					<p>
-						{products[2].brand}
-						{products[2].name}
+						{product.brand}
+						{product.name}
 					</p>
 				</div>
 				<p className={styles.article}>
-					Артикул: <span>{products[2].article}</span>
+					Артикул: <span>{product.article}</span>
 				</p>
 				<div className={styles.nameContainer}>
-					<p className={styles.brand}>{products[2].brand} </p>
-					<h1 className={styles.title}>{products[2].name}</h1>
+					<p className={styles.brand}>{product.brand} </p>
+					<h1 className={styles.title}>{product.name}</h1>
 				</div>
 				<div className={styles.typeContainer}>
-					<p className={styles.type}>{products[2].type}</p>
-					<p className={styles.size}>{products[2].size}.</p>
+					<p className={styles.type}>{product.type}</p>
+					<p className={styles.size}>{product.size}.</p>
 				</div>
 
-				<p className={styles.description}>{products[2].description}</p>
+				<p className={styles.description}>{product.description}</p>
 				<p className={styles.warning}>
 					Рулоны не стандартной ширины и детали простой и сложной формы
 					изготавливаются под заказ
