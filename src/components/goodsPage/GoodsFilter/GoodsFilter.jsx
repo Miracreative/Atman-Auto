38088209c695fa75e218791ec29758204eee5aac
Nikita Averyanov <110771527+Racio-begin@users.noreply.github.com>
2024-09-25 10:56:25 +0300

    feat: to improve the layout to an adequate display on various screens

--- src/components/goodsPage/GoodsFilter/GoodsFilter.jsx
+++ src/components/goodsPage/GoodsFilter/GoodsFilter.jsx
@@ -12,21 +12,39 @@ import styles from './GoodsFilter.module.scss';
 const GoodsFilter = () => {
 	const [isMobile, setIsMobile] = useState(false);
 
-	const [isOpenFilter, setIsOpenFilter] = useState(false);
+	const [isOpenFilter, setIsOpenFilter] = useState(true);
 
 	const width = useWindowWidth();
 
 	useEffect(() => {
 		if (width <= 480) {
 			setIsMobile(true);
+			setIsOpenFilter(false);
 		} else {
 			setIsMobile(false);
+			setIsOpenFilter(true);
 		}
 	}, [width]);
 
+	// useEffect(() => {
+	// 	// setIsOpenFilter(true);
+	// 	if (isMobile === true) {
+	// 		setIsOpenFilter(false);
+	// 	}
+	// }, []);
+
+	console.log('isMobile', isMobile);
+	console.log('width', width);
+	console.log('isOpenFilter', isOpenFilter);
+
 	return (
 		<>
-			{!isMobile && <GoodsFilterPanel />}
+			{!isMobile && (
+				<GoodsFilterPanel
+					isOpenFilter={isOpenFilter}
+					setIsOpenFilter={setIsOpenFilter}
+				/>
+			)}
 
 			{isMobile && (
 				<div className={styles.filterContainer}>
