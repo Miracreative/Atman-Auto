88507e6617caf0b30c734ad71be198dc4121525a
Nikita Averyanov <110771527+Racio-begin@users.noreply.github.com>
2024-09-27 15:33:37 +0300

    feat: configure form validation in the Form component

--- src/components/formSection/Form/Form.jsx
+++ src/components/formSection/Form/Form.jsx
@@ -3,13 +3,34 @@
 import { useState } from 'react';
 import Image from 'next/image.js';
 
-import FormInput from '../FormInput/FormInput.jsx';
+import { useForm } from 'react-hook-form';
+
+// import FormInput from '../FormInput/FormInput.jsx';
+
+import { EMAIL_REGEXP, PHONE_REGEXP } from '@/constants/regexp.js';
 
 import attachmentIcon from '../../../../public/attachment-icon.svg';
 
 import styles from './Form.module.scss';
 
 const Form = ({ isOpen, onClose }) => {
+	const {
+		register,
+		handleSubmit,
+		// watch,
+		formState: { errors },
+	} = useForm({
+		defaultValues: {
+			firstName: '',
+			lastName: '',
+			phoneNumber: '',
+			email: '',
+			comment: '',
+		},
+	});
+
+	console.log(errors);
+
 	const [isChecked, setIsChecked] = useState(false);
 
 	const handleCheckboxChange = () => {
@@ -23,7 +44,12 @@ const Form = ({ isOpen, onClose }) => {
 			}`}
 			onClick={() => onClose(false)}
 		>
-			<form className={styles.form} onClick={(e) => e.stopPropagation()}>
+			<form
+				className={styles.form}
+				onClick={(e) => e.stopPropagation()}
+				onSubmit={handleSubmit((data) => console.log(data))}
+				noValidate
+			>
 				<button type="button" className={styles.buttonClose} onClick={onClose}>
 					<span></span>
 					<span></span>
@@ -31,13 +57,106 @@ const Form = ({ isOpen, onClose }) => {
 				<h2 className={styles.title}>
 					Напишите <span>нам</span>
 				</h2>
-				<div className={styles.inputs}>
+				{/* <div className={styles.inputs}>
 					<FormInput type="text" text="Имя" />
 					<FormInput type="text" text="Фамилия" />
 					<FormInput type="phone" text="Номер телефона" />
 					<FormInput type="email" text="Почта" />
 					<FormInput type="text" text="Комментарий" />
+				</div> */}
+				<div className={styles.inputs}>
+					<div className={styles.inputContainer}>
+						<label htmlFor="firstName">Имя</label>
+						<input
+							id="firstName"
+							{...register('firstName', {
+								required: 'Обязательное поле',
+								minLength: {
+									value: 2,
+									message: 'Минимальное количество символов: 2',
+								},
+							})}
+							className={styles.input}
+							type="text"
+							placeholder="Имя"
+						/>
+						<p className={styles.error}>
+							{errors.firstName?.message ? errors.firstName.message : ''}
+						</p>
+					</div>
+
+					<div className={styles.inputContainer}>
+						<label htmlFor="lastName">Фамилия</label>
+						<input
+							id="lastName"
+							{...register('lastName', {
+								required: 'Обязательное поле',
+								minLength: {
+									value: 2,
+									message: 'Минимальное количество символов: 2',
+								},
+							})}
+							className={styles.input}
+							type="text"
+							placeholder="Фамилия"
+						/>
+						<p className={styles.error}>
+							{errors.lastName?.message ? errors.lastName.message : ''}
+						</p>
+					</div>
+
+					<div className={styles.inputContainer}>
+						<label htmlFor="phoneNumber">Номер телефона</label>
+						<input
+							id="phoneNumber"
+							{...register('phoneNumber', {
+								required: 'Обязательное поле',
+								pattern: {
+									value: PHONE_REGEXP,
+									message: 'Некорректный номер телефона',
+								},
+							})}
+							className={styles.input}
+							type="phone"
+							placeholder="Номер телефона"
+						/>
+						<p className={styles.error}>
+							{errors.phoneNumber?.message ? errors.phoneNumber.message : ''}
+						</p>
+					</div>
+
+					<div className={styles.inputContainer}>
+						<label htmlFor="email">Почта</label>
+						<input
+							id="email"
+							{...register('email', {
+								required: 'Обязательное поле',
+								pattern: {
+									value: EMAIL_REGEXP,
+									message: 'Введите корректный адрес электронной почты',
+								},
+							})}
+							className={styles.input}
+							type="email"
+							placeholder="Почта"
+						/>
+						<p className={styles.error}>
+							{errors.email?.message ? errors.email.message : ''}
+						</p>
+					</div>
+
+					<div className={styles.inputContainer}>
+						<label htmlFor="comment">Комментарий</label>
+						<input
+							id="comment"
+							{...register('comment', { required: false })}
+							className={styles.input}
+							type="text"
+							placeholder="Комментарий"
+						/>
+					</div>
 				</div>
+
 				<div className={styles.buttons}>
 					<button
 						className={`${styles.submitButton} button`}
