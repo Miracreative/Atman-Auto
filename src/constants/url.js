bfd27ad93ec2519ddd6d413bb1bd021670e9e490
Nikita Averyanov <110771527+Racio-begin@users.noreply.github.com>
2024-10-09 10:52:33 +0300

    feat: prepare constants for Product requests, remove unnecessary code and component

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,4 +1,13 @@
 // const URL = 'https://66daaa43f47a05d55be57df5.mockapi.io/atman/products';
-const URL = 'http://192.168.0.153:5000/api/goods';
 
-export default URL;
+const ALL_GOODS_URL = 'http://192.168.0.153:5000/api/goods';
+const FILTERED_MAIN_PARAM_GOODS_URL =
+	'http://192.168.0.153:5000/api/goods-main';
+const FILTERED_ALL_PARAM_GOODS_URL =
+	'http://192.168.0.153:5000/api/goods-parameters';
+
+export {
+	ALL_GOODS_URL,
+	FILTERED_MAIN_PARAM_GOODS_URL,
+	FILTERED_ALL_PARAM_GOODS_URL,
+};
