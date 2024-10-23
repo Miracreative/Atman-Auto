bd939c9d96abc4561f4773fc4527eeadd6316daa
Lonly <alexleonov011@gmail.com>
2024-10-23 19:12:28 +0300

    small fix

--- src/components/afoamPage/AdvantagesAfoam/AdvantagesAfoam.jsx
+++ src/components/afoamPage/AdvantagesAfoam/AdvantagesAfoam.jsx
@@ -1,51 +1,52 @@
-// import styles from './AdvantagesAfoam.module.scss';
-// import applicationData from '@/data/aplicationData';
+import styles from './AdvantagesAfoam.module.scss';
+import afoamData from '@/data/afoamData.js';
 
 
 
-// export default function AdvantagesAfoam() {
-// 	const { automobileIndustry } = applicationData;
-// 	const section = automobileIndustry.sections;
+
+export default function AdvantagesAfoam({ advantages }) {
+	const { automobileIndustry } = applicationData;
+	const section = automobileIndustry.sections;
 
 
 
-// 	return (
-// 		<>
-// 			<section className={styles.section}>
-// 				<div className={styles.container}>
-// 					<div className={styles.wrap}>
+	return (
+		<>
+			<section className={styles.section}>
+				<div className={styles.container}>
+					<div className={styles.wrap}>
 
 
-// 						<div className={styles.advantages}>
-// 							<div className={styles.innerAdvantages}>
+						<div className={styles.advantages}>
+							<div className={styles.innerAdvantages}>
 
 
-// 								<div className={styles.content}>
-// 									<h3 className={styles.title}>Преимущества <br /> использования:</h3>
+								<div className={styles.content}>
+									<h3 className={styles.title}>Преимущества <br /> использования:</h3>
 
-// 									<ul className={styles.advantagesList}>
-// 										{section.approach.advantages.map((advantage, index) => (
+									<ul className={styles.advantagesList}>
+										{afoamData.map((advantage, index) => (
 
-// 											<li key={index} className={styles.advantagesListItem}>
-// 												<div className={styles.advantagesPoint}>
-// 													{advantage}
-// 												</div>
-// 											</li>
+											<li key={index} className={styles.advantagesListItem}>
+												<div className={styles.advantagesPoint}>
+													{advantage}
+												</div>
+											</li>
 
-// 										))}
-// 									</ul>
-// 								</div>
-
-
-// 							</div>
-// 						</div>
+										))}
+									</ul>
+								</div>
 
 
-// 					</div>
-// 				</div>
-// 			</section>
-// 		</>
-// 	);
-// }
+							</div>
+						</div>
+
+
+					</div>
+				</div>
+			</section>
+		</>
+	);
+}
 
 
