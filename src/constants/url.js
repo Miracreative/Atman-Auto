c936f0b87a5b6c072508bfda28b4e7eac5d15042
Nikita Averyanov <110771527+Racio-begin@users.noreply.github.com>
2024-11-07 11:22:09 +0300

    test: test next prefix

--- src/constants/url.js
+++ src/constants/url.js
@@ -4,12 +4,48 @@
 //*
 //*
 
-//* HOST *//
-export const HOST = process.env.HOST;
-// export const HOST = 'http://83.147.246.205:5000';
+// //* HOST *//
+// export const HOST = process.env.HOST;
+// // export const HOST = 'http://83.147.246.205:5000';
 
-//* DOMAIN *//
-export const DOMAIN = process.env.DOMAIN;
+// //* DOMAIN *//
+// export const DOMAIN = process.env.DOMAIN;
+
+// //* Nodemailer *//
+// export const EMAIL_HOST = process.env.EMAIL_HOST;
+// export const EMAIL_PORT = process.env.EMAIL_PORT;
+// export const EMAIL_USER = process.env.EMAIL_USER;
+// export const EMAIL_PASS = process.env.EMAIL_PASS;
+// export const DESTINATION_EMAIL = process.env.DESTINATION_EMAIL;
+
+/////////////////////////////////////////////////////////////////////
+
+//*
+//*
+//* NEXT_PUBLIC переменные *//
+//*
+//*
+
+// * NEXT_PUBLIC_HOST *//
+export const HOST = process.env.NEXT_PUBLIC_HOST;
+
+// * NEXT_PUBLIC_DOMAIN *//
+export const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN;
+
+// * NEXT_PUBLIC Nodemailer *//
+export const EMAIL_HOST = process.env.NEXT_PUBLIC_EMAIL_HOST;
+export const EMAIL_PORT = process.env.NEXT_PUBLIC_EMAIL_PORT;
+export const EMAIL_USER = process.env.NEXT_PUBLIC_EMAIL_USER;
+export const EMAIL_PASS = process.env.NEXT_PUBLIC_EMAIL_PASS;
+export const DESTINATION_EMAIL = process.env.NEXT_PUBLIC_DESTINATION_EMAIL;
+
+/////////////////////////////////////////////////////////////////////
+
+//*
+//*
+//* Переменные путей *//
+//*
+//*
 
 //* Базовый URL *//
 const BASE_URL = `${HOST}/api`;
@@ -28,34 +64,6 @@ export const URL_NEWS = `${BASE_URL}/news`;
 
 //* URL СОУТ *//
 export const SOUT_URL = `${BASE_URL}/sout`;
-
-//* Nodemailer *//
-export const EMAIL_HOST = process.env.EMAIL_HOST;
-export const EMAIL_PORT = process.env.EMAIL_PORT;
-export const EMAIL_USER = process.env.EMAIL_USER;
-export const EMAIL_PASS = process.env.EMAIL_PASS;
-export const DESTINATION_EMAIL = process.env.DESTINATION_EMAIL;
-
-/////////////////////////////////////////////////////////////////////
-
-//*
-//*
-//* NEXT_PUBLIC переменные *//
-//*
-//*
-
-//* NEXT_PUBLIC_HOST *//
-// export const HOST = process.env.NEXT_PUBLIC_HOST;
-
-//* NEXT_PUBLIC_DOMAIN *//
-// export const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN;
-
-//* Nodemailer *//
-// export const EMAIL_HOST = process.env.NEXT_PUBLIC_EMAIL_HOST;
-// export const EMAIL_PORT = process.env.NEXT_PUBLIC_EMAIL_PORT;
-// export const EMAIL_USER = process.env.NEXT_PUBLIC_EMAIL_USER;
-// export const EMAIL_PASS = process.env.NEXT_PUBLIC_EMAIL_PASS;
-// export const DESTINATION_EMAIL = process.env.NEXT_PUBLIC_DESTINATION_EMAIL;
 
 /////////////////////////////////////////////////////////////////////
 
@@ -140,3 +148,5 @@ console.log(
 // );
 
 //* TESTING DESTINATION_EMAIL *//
+
+/////////////////////////////////////////////////////////////////////
